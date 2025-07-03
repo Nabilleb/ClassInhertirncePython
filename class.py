dffd0b4b4b52dea398de6ad4d8cf117cb7063af9
Nabilleb <nabilgreen500@gmail.com>
2025-07-03 15:21:43 +0300

    can read email and name

--- class.py
+++ class.py
@@ -2,3 +2,7 @@ class User:
     def __init__(self, name, email):
         self.__name = name
         self.__email = email
+
+    
+    def get_info(self):
+        return f"Name: {self.__name}, Email: {self.__email}"
